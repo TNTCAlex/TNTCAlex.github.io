d248d79cb2eb5330e88ec6461bc50fd2a52ae5e1
TNTC <thienchuongtn1203@gmail.com>
2026-01-18 00:22:10 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const about = document.querySelector('.about');
+
+const observer = new IntersectionObserver(
+  ([entry]) => {
+    if (entry.isIntersecting) {
+      about.classList.add('active');
+    }
+  },
+  { threshold: 0.6 }
+);
+
+observer.observe(about);
