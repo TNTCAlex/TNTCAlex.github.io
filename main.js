421570fe643625c71bd0c5059f450942175aab05
TNTC <thienchuongtn1203@gmail.com>
2026-01-18 01:11:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ const observer = new IntersectionObserver(
   ([entry]) => {
     if (entry.isIntersecting) {
       about.classList.add('active');
-    } else {
-      about.classList.remove('active');
-    }
   },
   { threshold: 0.6 }
 );
